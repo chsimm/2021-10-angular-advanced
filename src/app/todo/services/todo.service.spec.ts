f3c6749ff11ff66b34d1a2b30697a50e62de497a
FabianGosebrink <fabian.gosebrink@outlook.com>
2021-10-18 16:36:45 +0200

    adding tests

--- src/app/todo/services/todo.service.spec.ts
+++ src/app/todo/services/todo.service.spec.ts
@@ -1,15 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { RestService } from './../../shared/rest/rest.service';
+import { RestServiceMock } from './../../shared/rest/rest.service-mock';
 import { TodoService } from './todo.service';
 
 fdescribe('TodoService', () => {
   let service: TodoService;
+  let restService: RestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: RestService, useClass: RestServiceMock }],
+    });
     service = TestBed.inject(TodoService);
+    restService = TestBed.inject(RestService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getAllTodos', () => {
+    it(
+      'should be called with correct url',
+      waitForAsync(() => {
+        const spy = spyOn(restService, 'get').and.returnValue(of(null));
+
+        service.getAllTodos().subscribe(() => {
+          expect(spy).toHaveBeenCalledOnceWith(
+            'https://sampletodobackend.azurewebsites.net/api/v1/todos/'
+          );
+        });
+      })
+    );
   });
 });
